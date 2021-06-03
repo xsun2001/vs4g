b4b234b799918df10e7ed74f7a047b06a77552de
shanjb0221 <2533296339@qq.com>
2021-06-03 22:42:04 +0800

    refactor(algo network flow): finish all

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -13,17 +13,25 @@ import { Ford } from "@/algorithms/SSSP/Ford";
 import { SalesmanPath } from "@/algorithms/TravelingSalesmanProblem/TravelingSalesmanProb";
 import { SalesmanCheaperAlgo } from "@/algorithms/TravelingSalesmanProblem/SalesmanCheaperAlgo";
 
-import { FordFulkerson } from "@/algorithms/networkflow/FordFulkerson";
-import { EdmondsKarp } from "@/algorithms/networkflow/EdmondsKarp";
-import { Dinic } from "@/algorithms/networkflow/Dinic";
-import { MinCostFlow } from "@/algorithms/networkflow/MinCostFlow";
-import { ZkwMCF } from "@/algorithms/networkflow/ZkwMCF";
 import { HungarianDFS } from "@/algorithms/matching/BipartiteMatching";
 import { KuhnMunkres } from "@/algorithms/matching/WeightedBipartiteMatching";
 import { Gabow } from "@/algorithms/matching/Matching";
 import { DMP } from "@/algorithms/planargraph/DMP";
 
-const newAlgorithms: NewGraphAlgorithm[] = [new NewDijkstra()];
+import { NewFordFulkerson } from "@/algorithms/networkflow/FordFulkerson";
+import { NewEdmondsKarp } from "@/algorithms/networkflow/EdmondsKarp";
+import { NewDinic } from "@/algorithms/networkflow/Dinic";
+import { NewMinCostFlow } from "@/algorithms/networkflow/MinCostFlow";
+import { NewZkwMCF } from "@/algorithms/networkflow/ZkwMCF";
+
+const newAlgorithms: NewGraphAlgorithm[] = [
+  new NewDijkstra(),
+  new NewEdmondsKarp(),
+  new NewFordFulkerson(),
+  new NewDinic(),
+  new NewMinCostFlow(),
+  new NewZkwMCF()
+];
 
 const algorithms = new Map<string, () => GraphAlgorithm>([
   ["BFS", () => new BfsFindPath()],
@@ -37,11 +45,6 @@ const algorithms = new Map<string, () => GraphAlgorithm>([
   ["Ford", () => new Ford()],
   ["SalesmanProblem", () => new SalesmanPath()],
   ["SalesmanCheaperAlgorithm", () => new SalesmanCheaperAlgo()],
-  ["mf_ff", () => new FordFulkerson()],
-  ["mf_ek", () => new EdmondsKarp()],
-  ["mf_dinic", () => new Dinic()],
-  ["mcf_classic", () => new MinCostFlow()],
-  ["mcf_zkw", () => new ZkwMCF()],
   ["mbm_hungarian", () => new HungarianDFS()],
   ["mwbm_km", () => new KuhnMunkres()],
   ["mm_gabow", () => new Gabow()],
