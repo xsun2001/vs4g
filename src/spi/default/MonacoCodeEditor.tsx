01ff349676731be4c4f59086e30cd95cc0d05edf
shanjb0221 <2533296339@qq.com>
2021-06-06 17:37:02 +0800

    fix(ui): forbid grammar check in MonacoCodeEditor

--- src/spi/default/MonacoCodeEditor.tsx
+++ src/spi/default/MonacoCodeEditor.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 import "./MonacoEditorPatcher.css";
 
-const MonacoCodeEditor: React.FC<{ value: string, onChange: (value: string) => void }> = props => (
-  <MonacoEditor value={props.value} onChange={props.onChange} height={200} options={{
-    minimap: { enabled: false }
-  }} />
+const MonacoCodeEditor: React.FC<{ value: string; onChange: (value: string) => void }> = props => (
+  <MonacoEditor
+    value={props.value}
+    onChange={props.onChange}
+    height={200}
+    language={"plain"}
+    options={{
+      minimap: { enabled: false }
+    }}
+  />
 );
 
 export default MonacoCodeEditor;
